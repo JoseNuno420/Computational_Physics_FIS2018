b382d92637404826964777e0a2a24cacbe124434
José Nuno Campos <101421302+JoseNuno420@users.noreply.github.com>
2022-03-16 12:40:11 +0000

    Create 1 - Factorial of given number.py

---
+++ Chapter 3 - Accuracy and Speed/1 - Factorial of given number.py
@@ -0,0 +1,13 @@
+import math
+
+def factorial(n):
+    n = int(input())
+    return math.factorial(n)
+def factorial_float(n):
+    n = float(input())
+    return math.factorial(n)
+
+
+print(factorial(200))
+print()
+print(factorial_float(100.0))
