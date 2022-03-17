e105ce2b7073343c214f8af6e95d8a8ae50d365d
José Nuno Campos <101421302+JoseNuno420@users.noreply.github.com>
2022-03-17 10:38:09 +0000

    Update 7 - Mandelbrot set graphical representation (using plt.imshow).py

--- Chapter 2 - Graphic representation of experimental data/7 - Mandelbrot set graphical representation (using plt.imshow).py
+++ Chapter 2 - Graphic representation of experimental data/7 - Mandelbrot set graphical representation (using plt.imshow).py
@@ -1,1 +1,15 @@
+import numpy as np
+import matplotlyb.pyplot as plt
 
+x,y = np.mgrid[-2:2:100j,-2:2:100j]
+
+c = x + y*1j
+
+def mandelbort(c,n=100):
+    z = 0
+    for i in range(n):
+        z = c + z**2
+    z2 = np.sqrt(z*z.conjugate())
+    return z2 < 2
+
+plt.imshow(mandelbort(c),plt.cm.jet)
