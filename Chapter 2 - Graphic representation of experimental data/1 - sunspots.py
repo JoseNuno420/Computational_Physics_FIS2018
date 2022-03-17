4d9666093265ebe82e09549def325ec67c02a4aa
José Nuno Campos <101421302+JoseNuno420@users.noreply.github.com>
2022-03-17 10:05:58 +0000

    Update 1 - sunspots.py

--- Chapter 2 - Graphic representation of experimental data/1 - sunspots.py
+++ Chapter 2 - Graphic representation of experimental data/1 - sunspots.py
@@ -1,8 +1,32 @@
 import numpy as np
-dados = np.loadtxt(r"sunspots.txt")
+data = np.loadtxt(r"sunspots.txt")
 
-x = dados[:,0]
-y = dados[:,1]
+x = data[:,0]
+y = data[:,1]
 
 plt.plot(x,y)
+plt.show()
+
+# (2)
+
+X = data[:1001,0]
+Y = data[:1001,1]
+
+plt.plot(X,Y)
+plt.show()
+
+# (3)
+
+X = data[:1001,0]
+Y = data[:1001,1]
+
+
+def average(y):
+    r = 5
+    for m in range(-r,r+1):
+        sum = y + m
+    return (1/(2*r+1)* sum)
+
+plt.plot(X,Y) # blue graph
+plt.plot(X,average(Y)) #orange graph
 plt.show()
