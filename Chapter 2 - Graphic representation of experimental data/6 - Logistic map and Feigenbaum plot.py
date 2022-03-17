79a4ac5982364945fb258509816138a2c9a72ecf
José Nuno Campos <101421302+JoseNuno420@users.noreply.github.com>
2022-03-17 10:36:58 +0000

    Update 6 - Logistic map and Feigenbaum plot.py

--- Chapter 2 - Graphic representation of experimental data/6 - Logistic map and Feigenbaum plot.py
+++ Chapter 2 - Graphic representation of experimental data/6 - Logistic map and Feigenbaum plot.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlyb.pyplot as plt
 
+values = np.arange(1,4,0.01)
+x = np.full_like(values,0.5)
+
+for i in values:
+    x = values*x*(1-x)
+    plt.scatter(values,x,s=0.1)
+plt.ylabel("x")
+plt.xlabel("r")
+
+plt.show()
