e47aa621465c40cbdd65df4c9e19f50c0836b755
José Nuno Campos <101421302+JoseNuno420@users.noreply.github.com>
2022-03-17 10:22:25 +0000

    Update 3 - stm.py

--- Chapter 2 - Graphic representation of experimental data/3 - stm.py
+++ Chapter 2 - Graphic representation of experimental data/3 - stm.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+data = np.loadtxt(r"stm.txt")
+plt.imshow(data,origin="lower") #density plot
+plt.show()
+
+x = data[:,0]
+y = data[:,1]
+plt.plot(x,y) # regular xy plot
+plt.show()
