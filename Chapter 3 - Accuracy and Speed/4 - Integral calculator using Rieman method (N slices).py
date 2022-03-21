04024bd0b42e66614898bb5d1aa7183a5537670c
José Nuno Campos <101421302+JoseNuno420@users.noreply.github.com>
2022-03-21 08:52:53 +0000

    Update 4 - Integral calculator using Rieman method (N slices).py

--- Chapter 3 - Accuracy and Speed/4 - Integral calculator using Rieman method (N slices).py
+++ Chapter 3 - Accuracy and Speed/4 - Integral calculator using Rieman method (N slices).py
@@ -1,15 +1,14 @@
 import numpy as np
+
 def f(x):
     return np.sqrt(1-x**2)
 
-def integral(slices):
-    total = 0.0
-    h = 2/slices
-    for i in range(slices):
-        x_i = -1+i*h
-        total += f(x_i)*i
-    return total
-    
-print(integral(300000))
+def integrar(N):
+    soma = 0
+    h = 2/N
+    for k in range(N):
+        x_k = -1 + h*k
+        soma += f(x_k)*h
+    return soma
+print(integrar(100000)
 print(np.pi/2)
-%timeit -r 1 -n 1 integral(300000) #nmaximum number of slices to have the calculation under 1 second of duration
