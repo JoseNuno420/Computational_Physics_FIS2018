dbc9da7e8ef14b13f2453b32bf3e1aba2f904405
José Nuno Campos <101421302+JoseNuno420@users.noreply.github.com>
2024-03-23 22:38:06 +0000

    Solution

--- Chapter 5 - Linear and Non-Linear Equations/1.py
+++ Chapter 5 - Linear and Non-Linear Equations/1.py
@@ -1,1 +1,22 @@
+A = np.array([[ 4,-1,-1,-1 ],[ -1,0,3,0 ],[ -1,3,0,-1 ],[ -1,-1,0,4]], float)
+v = np.array([ 5, 5, 0, 0 ],float)
+N = len(v)
 
+for m in range(N):
+    div = A[m,m]
+    A[m,:] /= div
+    v[m] /= div
+    for i in range(m+1,N):
+        mult = A[i,m]
+        A[i,:] -= mult*A[m,:]
+        v[i] -= mult*v[m]
+
+x = np.empty(N,float)
+for m in range(N-1,-1,-1):
+    x[m] = v[m]
+    for i in range(m+1,N):
+        x[m] -= A[m,i]*x[i]
+
+print(x)
+#para verificar utilizamos o solve do numpy.linalg
+print(solve(A,v))
